90b665f51b148f082a0100410849d4913559886d
Strinx13 <jhonnyayala355@gmail.com>
2024-10-14 11:51:58 -0600

    Prueba3

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ import authController from './controllers/authControllers.js';
 const app = express();
 
 // Definir las IPs o dominios permitidos
-const allowedOrigins = ['http://192.168.5.0'];
+const allowedOrigins = ['35.160.120.126'];
 
 // Configurar las opciones de CORS
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin)) {
-          callback(null, true);  // Permitir si el origen está en la lista
-        } else {
-          callback(new Error('Acceso no permitido por CORS'));  // Bloquear si no está en la lista
-        }
-      },
+  origin: function (origin, callback) {
+    // Si no hay origen (por ejemplo, en solicitudes desde herramientas como Postman) o está en la lista de permitidos
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);  // Se permite la solicitud
+    } else {
+      callback(new Error('Acceso no permitido por CORS'));  // Se rechaza la solicitud
+    }
+  },
   optionsSuccessStatus: 204 // Estado para respuestas pre-flight (OPCIONAL)
 };
 
